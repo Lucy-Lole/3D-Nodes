eb858d6645c20a86764db7d8d42797c2c325b65c
Lucy Lole <llole.62591@farnborough.ac.uk>
2018-06-27 03:29:53 +0100

    Initial commit

---
+++ src/Node.java
@@ -0,0 +1,26 @@
+public class Node {
+    public double x,y,z;
+
+    public double size,sizeModifier;
+
+    public double xSpeed,ySpeed,zSpeed;
+
+    Node(double x, double y, double z, double size, double sizeModifier, double xSpeed, double ySpeed, double zSpeed) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.size = size;
+        this.sizeModifier = sizeModifier;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+        this.zSpeed = zSpeed;
+    }
+
+    public void updatePosition() {
+        x += xSpeed;
+        y += ySpeed;
+        z += zSpeed;
+
+    }
+
+}
