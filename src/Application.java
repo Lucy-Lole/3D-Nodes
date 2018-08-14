8fedc04e3146eea1f6ccd6f7cbcca5aafd431585
Lucy Lole <llole.62591@farnborough.ac.uk>
2018-08-14 16:04:05 +0100

    Lots of work on the main application; Screen controller class created

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,15 @@
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.*;
+import javafx.scene.paint.Color;
 import javafx.stage.*;
+
+import java.awt.*;
 import java.util.HashSet;
 import javafx.animation.*;
 
@@ -9,9 +17,10 @@ import javafx.animation.*;
 public class Application {
 
 
-    static HashSet<KeyCode> keysPressed = new HashSet<>();
+    final static double WIN_HEIGHT = 400;
+    final static double WIN_WIDTH = 600;
 
-
+    static HashSet<double[]> mouseClicks = new HashSet<>();
 
 
     public static void main(String args[]) {
@@ -21,6 +30,44 @@ public class Application {
 
 
     private static void start() {
-        
+        System.out.println("Application starting...");
+        System.out.println("Initializing Screen Controller");
+
+        ScreenController controller = new ScreenController();
+
+        Group root = new Group();
+        Stage mainStage =  new Stage();
+        Scene mainScene = new Scene(root);
+        Canvas canvas = new Canvas();
+
+        mainStage.setResizable(false);
+        mainStage.setHeight(WIN_HEIGHT);
+        mainStage.setWidth(WIN_WIDTH);
+        mainStage.setTitle("3D Nodes");
+        mainStage.setScene(mainScene);
+        mainStage.show();
+
+        mainStage.setOnCloseRequest(event -> Application.close());
+
+        canvas.setWidth(WIN_WIDTH);
+        canvas.setHeight(WIN_HEIGHT);
+        GraphicsContext GC = canvas.getGraphicsContext2D();
+
+        canvas.setOnMouseClicked(event ->
+                mouseClicks.add(new double[] {event.getX(), event.getY()}));
+
+        root.getChildren().add(canvas);
+        GC.setFill(Color.BLACK);
+
+
+
+    }
+
+    private static void close() {
+        System.out.println("Application closing");
+        for (double[] d : mouseClicks) {
+            System.out.println(d[0] +" "+ d[1]);
+        }
+        System.exit(0);
     }
 }
